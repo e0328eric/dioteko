e1f4e017c449d0904ff722f2e3f58ea3a1489b98
Sungbae Jeong <almagest0328@gmail.com>
2022-05-23 17:05:10 +0900

    change branch of main raylib

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
 use dioteko::prelude::*;
 use dioteko::text;
-use dioteko::textures::texture::Texture;
 
 const SCREEN_WIDTH: usize = 800;
 const SCREEN_HEIGHT: usize = 450;
 
 fn main() -> dioteko::Result<()> {
-    let window = WindowBuilder::new(
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        "raylib [texture] example -texture rectangle",
-    )
-    .build()?;
-
-    let texture = Texture::load("image/test.png");
+    let window = WindowBuilder::new(SCREEN_WIDTH, SCREEN_HEIGHT, "dioteko example").build()?;
 
     while !window.should_close() {
         let painter = Painter::begin();
 
         painter.clear_background(BLANK);
 
-        texture.draw_texture(
-            (SCREEN_WIDTH as i32 - texture.width()) / 5,
-            (SCREEN_HEIGHT as i32 - texture.height()) / 5,
-            WHITE,
-        );
         text::draw_text(
             "from https://pixabay.com/photos/ink-feather-pen-quill-write-5067880/",
             50,
